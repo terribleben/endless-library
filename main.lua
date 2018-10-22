5b7429c602dd786df8926f9248317cce8dba44d0
Ben Roth <spongebucket@gmail.com>
2018-10-22 15:55:29 -0700

    empty lua project

---
+++ main.lua
@@ -0,0 +1,14 @@
+
+function love.load()
+end
+
+function love.draw()
+   love.graphics.setColor(1, 1, 1, 1)
+   love.graphics.circle('fill', 0, 0, 100)
+end
+
+function love.update(dt)
+end
+
+function love.keypressed(key, ...)
+end

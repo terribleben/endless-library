58a031547e4c0059ff9e5ac98bfacb3f92dd7c97
Ben Roth <spongebucket@gmail.com>
2018-10-22 16:00:14 -0700

    capture viewport dims on init

--- main.lua
+++ main.lua
@@ -1,14 +1,29 @@
+local SharedState = require 'sharedstate'
 
 function love.load()
+   _reset()
 end
 
 function love.draw()
+   love.graphics.push()
+   -- transform to viewport coords
+   love.graphics.translate(SharedState.screen.width * 0.5, SharedState.screen.height * 0.5)
+   love.graphics.translate(SharedState.viewport.x, SharedState.viewport.y)
    love.graphics.setColor(1, 1, 1, 1)
-   love.graphics.circle('fill', 0, 0, 100)
+   love.graphics.rectangle(
+      'line',
+      0, 0,
+      SharedState.viewport.width, SharedState.viewport.height
+   )
+   love.graphics.pop()
 end
 
 function love.update(dt)
 end
 
 function love.keypressed(key, ...)
+end
+
+function _reset()
+   SharedState:reset()
 end
---
+++ sharedstate.lua
@@ -0,0 +1,16 @@
+SharedState = {
+   screen = { width = 0, height = 0 },
+   viewport = { x = 0, y = 0, width = 0, height = 0 },
+}
+
+function SharedState:reset()
+   local width, height, flags = love.window.getMode()
+   self.screen.width = width
+   self.screen.height = height
+   self.viewport.width = math.min(800, self.screen.width)
+   self.viewport.height = math.min(600, self.screen.height)
+   self.viewport.x = self.viewport.width * -0.5
+   self.viewport.y = self.viewport.height * -0.5
+end
+
+return SharedState

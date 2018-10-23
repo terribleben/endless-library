96770a8349f7b9112644fb554e75f3b74bae5d34
Ben Roth <spongebucket@gmail.com>
2018-10-23 10:50:19 -0700

    add primitive book slanting

--- book.lua
+++ book.lua
@@ -2,6 +2,7 @@ Book = {
    position = { x = 0, y = 0 },
    width = 0,
    height = 0,
+   angle = 0,
    PULP_HEIGHT = 30,
    PAPERBACK_HEIGHT = 42,
 }
@@ -16,7 +17,7 @@ function Book:draw()
    love.graphics.setColor(1, 1, 1, 1)
    love.graphics.push()
    love.graphics.translate(self.position.x, self.position.y)
-   -- TODO: love.graphics.rotate()
+   love.graphics.rotate(self.angle)
    love.graphics.rectangle(
       'line',
       -self.width * 0.5, -self.height * 0.5,
--- shelf.lua
+++ shelf.lua
@@ -31,19 +31,29 @@ end
 
 function Shelf:_reset()
    local x = 9 + love.math.random(8)
+   local angle, prevAngle = 0, 0
    self._books = {}
    self._numBooks = 0
    -- TODO: 18 is currently max book width, centralize
    while x < self.width - 18 do
       local width, height = self:_getBookDimensions()
+      angle = self:_getBookAngle(angle)
+      local absAngle = math.abs(angle)
+      local boundsWidth, boundsHeight =
+         width * math.cos(absAngle) + height * math.sin(absAngle),
+         width * math.sin(absAngle) + height * math.cos(absAngle)
+      local nextX
+      if prevAngle == angle then nextX = x + width * 0.5 else nextX = x + boundsWidth * 0.5 end
       self._numBooks = self._numBooks + 1
       self._books[self._numBooks] =
          Book:new({
-               position = { x = x, y = self.height - height },
+               position = { x = nextX, y = self.height - boundsHeight * 0.5 },
                width = width,
                height = height,
+               angle = angle,
          })
-      x = x + width
+      x = x + boundsWidth
+      prevAngle = angle
    end
 end
 
@@ -62,6 +72,27 @@ function Shelf:_getBookDimensions()
       height = self.height * (0.6 + 0.1 * love.math.random(3))
    end
    return width, height
+end
+
+function Shelf:_getBookAngle(prevAngle)
+   local pSwitch = love.math.random()
+   if prevAngle == 0 then
+      if pSwitch < 0.25 then
+         -- start leaning
+         return -0.1 + love.math.random() * 0.2
+      else
+         -- continue upright
+         return 0
+      end
+   else
+      if pSwitch < 0.5 then
+         -- stop leaning
+         return 0
+      else
+         -- continue leaning
+         return prevAngle
+      end
+   end
 end
 
 return Shelf

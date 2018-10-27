83c21aefa0b957afd4a6e3407a1ecd8aec07efdf
Ben Roth <spongebucket@gmail.com>
2018-10-26 21:53:41 -0700

    decompose rooms into zones

--- room.lua
+++ room.lua
@@ -1,27 +1,22 @@
 local SharedState = require 'sharedstate'
-local Bookcase = require 'bookcase'
-local Desk = require 'desk'
 local Exit = require 'exit'
 local Map = require 'map'
 local Window = require 'window'
+local Zone = require 'zone'
 
 Room = {
-   _bookcases = {},
-   _numBookcases = 0,
-   _desks = {},
-   _numDesks = 0,
-   _windows = {},
-   _numWindows = 0,
    exits = nil,
    width = 0,
    height = 0,
+   _zones = nil,
+   _numZones = 0,
+   _windows = nil,
+   _numWindows = 0,
 }
 
 function Room:reset()
-   self._bookcases = {}
-   self._numBookcases = 0
-   self._desks = {}
-   self._numDesks = 0
+   self._zones = {}
+   self._numZones = 0
    self._windows = {}
    self._numWindows = 0
    self.exits = {}
@@ -40,20 +35,15 @@ function Room:reset()
    end
    self.height = SharedState.viewport.height
    
-   local pRoomLayout = love.math.random()
+   -- local pRoomLayout = love.math.random()
    -- todo: formalize room layouts a bit more
-   if pRoomLayout < 0.3 then
-      self:_addUniformBookcases(0, self.width)
-   elseif pRoomLayout < 0.7 then
-      self:_addRandomBookcases(0, self.width, true, true)
-   elseif pRoomLayout < 0.8 then
-      local x, width = self:_addDesk()
-      self:_addRandomBookcases(0, x, true)
-      self:_addRandomBookcases(x + width, self.width, true)
-   else
-      self:_addRandomBookcases(0, self.width, false, true)
-   end
-
+   -- todo: multiple zones
+   self:_addZone({
+         position = { x = 0, y = 0 },
+         width = self.width,
+         height = self.height,
+   })
+ 
    local pWindowLayout = love.math.random()
    if pWindowLayout < 0.1 then
       self:_addSingleWindow()
@@ -88,12 +78,9 @@ function Room:draw()
    for ii = 1, self._numWindows do
       self._windows[ii]:draw()
    end
-   for ii = 1, self._numBookcases do
-      self._bookcases[ii]:draw()
+   for ii = 1, self._numZones do
+      self._zones[ii]:draw()
    end
-   for ii = 1, self._numDesks do
-      self._desks[ii]:draw()
-   end   
 end
 
 function Room:_addSingleWindow()
@@ -132,82 +119,11 @@ end
 function Room:_addWindow(w)
    self._numWindows = self._numWindows + 1
    self._windows[self._numWindows] = Window:new(w)
-   return x, width
 end
 
-function Room:_addDesk()
-   local width, height = 200, 75
-   local x = love.math.random() * (self.width - width)
-   self._numDesks = self._numDesks + 1
-   self._desks[self._numDesks] = Desk:new({
-         position = { x = x, y = self.height - height },
-         width = width,
-         height = height,
-   })
-   return x, width
-end
-
-function Room:_addUniformBookcases(xBegin, xEnd)
-   local width, height = Bookcase.randomWidth(), 150 + 25 * love.math.random(4)
-   local spacing = 5 * math.random(0, 6)
-   local numBookcases = math.floor((xEnd - xBegin) / (width + spacing))
-
-   -- append numBookcases new cases
-   for ii = 1, numBookcases do
-      self._numBookcases = self._numBookcases + 1
-      self._bookcases[self._numBookcases] = Bookcase:new({
-         position = { x = 0, y = self.height - height },
-         width = width,
-         height = height,
-      })
-   end
-
-   -- space evenly
-   local totalWidth = (numBookcases * width) + ((numBookcases - 1) * spacing)
-   local xOffset = ((xEnd - xBegin) - totalWidth) * 0.5
-   for ii = 0, numBookcases - 1 do
-      self._bookcases[self._numBookcases - ii].position.x =
-         (ii * (width + spacing)) + xBegin + xOffset
-   end
-      
-end
-
-function Room:_addRandomBookcases(xBegin, xEnd, allowEmpty, isRoot)
-   local x, width = self:_addRandomBookcase(xBegin, xEnd, allowEmpty and not isRoot)
-   if width > 0 then
-      self:_addRandomBookcases(xBegin, x, allowEmpty)
-      self:_addRandomBookcases(x + width, xEnd, allowEmpty)
-   end
-end
-
--- add one bookcase somewhere between xBegin and xEnd
--- or none if nothing fits.
--- return the x, width of the resulting bookcase; or zero width if none is created.
-function Room:_addRandomBookcase(xBegin, xEnd, allowEmpty)
-   if allowEmpty and love.math.random() < 0.2 then
-      return xEnd, 0
-   end
-   if xEnd - xBegin < Bookcase.sizes.NARROW then
-      return xEnd, 0
-   end
-   
-   local x, width
-   repeat
-      width = Bookcase.randomWidth()
-   until xBegin + width < xEnd
-   
-   x = xBegin + love.math.random() * (xEnd - xBegin - width)
-
-   self._numBookcases = self._numBookcases + 1
-   local height = 150 + 25 * love.math.random(4)
-   self._bookcases[self._numBookcases] = 
-      Bookcase:new({
-            position = { x = x, y = self.height - height },
-            width = width,
-            height = height,
-      })
- 
-   return x, width
+function Room:_addZone(z)
+   self._numZones = self._numZones + 1
+   self._zones[self._numZones] = Zone:new(z)
 end
 
 return Room
---
+++ zone.lua
@@ -0,0 +1,134 @@
+local SharedState = require 'sharedstate'
+local Bookcase = require 'bookcase'
+local Desk = require 'desk'
+local Exit = require 'exit'
+local Map = require 'map'
+
+Zone = {
+   position = { x = 0, y = 0 },
+   width = 0,
+   height = 0,
+   _bookcases = {},
+   _numBookcases = 0,
+   _desks = {},
+   _numDesks = 0,
+}
+
+
+function Zone:new(t)
+   t = t or {}
+   setmetatable(t, { __index = self })
+   t:_reset()
+   return t
+end
+
+function Zone:_reset()
+   self._bookcases = {}
+   self._numBookcases = 0
+   self._desks = {}
+   self._numDesks = 0
+   
+   local pZoneLayout = love.math.random()
+   -- todo: formalize zone layouts a bit more
+   if pZoneLayout < 0.3 then
+      self:_addUniformBookcases(0, self.width)
+   elseif pZoneLayout < 0.7 then
+      self:_addRandomBookcases(0, self.width, true, true)
+   elseif pZoneLayout < 0.8 then
+      local x, width = self:_addDesk()
+      self:_addRandomBookcases(0, x, true)
+      self:_addRandomBookcases(x + width, self.width, true)
+   else
+      self:_addRandomBookcases(0, self.width, false, true)
+   end
+end
+
+function Zone:draw()
+   love.graphics.push()
+   love.graphics.translate(self.position.x, self.position.y)
+   -- TODO: draw floor
+   for ii = 1, self._numBookcases do
+      self._bookcases[ii]:draw()
+   end
+   for ii = 1, self._numDesks do
+      self._desks[ii]:draw()
+   end
+   love.graphics.pop()
+end
+
+function Zone:_addDesk()
+   local width, height = 200, 75
+   local x = love.math.random() * (self.width - width)
+   self._numDesks = self._numDesks + 1
+   self._desks[self._numDesks] = Desk:new({
+         position = { x = x, y = self.height - height },
+         width = width,
+         height = height,
+   })
+   return x, width
+end
+
+function Zone:_addUniformBookcases(xBegin, xEnd)
+   local width, height = Bookcase.randomWidth(), 150 + 25 * love.math.random(4)
+   local spacing = 5 * math.random(0, 6)
+   local numBookcases = math.floor((xEnd - xBegin) / (width + spacing))
+
+   -- append numBookcases new cases
+   for ii = 1, numBookcases do
+      self._numBookcases = self._numBookcases + 1
+      self._bookcases[self._numBookcases] = Bookcase:new({
+         position = { x = 0, y = self.height - height },
+         width = width,
+         height = height,
+      })
+   end
+
+   -- space evenly
+   local totalWidth = (numBookcases * width) + ((numBookcases - 1) * spacing)
+   local xOffset = ((xEnd - xBegin) - totalWidth) * 0.5
+   for ii = 0, numBookcases - 1 do
+      self._bookcases[self._numBookcases - ii].position.x =
+         (ii * (width + spacing)) + xBegin + xOffset
+   end
+      
+end
+
+function Zone:_addRandomBookcases(xBegin, xEnd, allowEmpty, isRoot)
+   local x, width = self:_addRandomBookcase(xBegin, xEnd, allowEmpty and not isRoot)
+   if width > 0 then
+      self:_addRandomBookcases(xBegin, x, allowEmpty)
+      self:_addRandomBookcases(x + width, xEnd, allowEmpty)
+   end
+end
+
+-- add one bookcase somewhere between xBegin and xEnd
+-- or none if nothing fits.
+-- return the x, width of the resulting bookcase; or zero width if none is created.
+function Zone:_addRandomBookcase(xBegin, xEnd, allowEmpty)
+   if allowEmpty and love.math.random() < 0.2 then
+      return xEnd, 0
+   end
+   if xEnd - xBegin < Bookcase.sizes.NARROW then
+      return xEnd, 0
+   end
+   
+   local x, width
+   repeat
+      width = Bookcase.randomWidth()
+   until xBegin + width < xEnd
+   
+   x = xBegin + love.math.random() * (xEnd - xBegin - width)
+
+   self._numBookcases = self._numBookcases + 1
+   local height = 150 + 25 * love.math.random(4)
+   self._bookcases[self._numBookcases] = 
+      Bookcase:new({
+            position = { x = x, y = self.height - height },
+            width = width,
+            height = height,
+      })
+ 
+   return x, width
+end
+
+return Zone
